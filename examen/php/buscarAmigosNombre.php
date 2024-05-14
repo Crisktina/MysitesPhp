c34f42d14d1497eb3c7890ce0f6016e6839c9ee9
Cristina <crishidalgolopez@gmail.com>
2024-05-14 12:22:49 +0200

    mensaje si no hay coincidencias de amigos

--- examen/php/buscarAmigosNombre.php
+++ examen/php/buscarAmigosNombre.php
@@ -7,24 +7,28 @@ if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["buscar"])){
               <?php
               // Mostrar lista de amigos almacenados en la sesión
               if(isset($_SESSION['amigos'])) {
-                foreach($_SESSION['amigos'][0] as $amigo) { 
+                foreach($_SESSION['amigos'][0] as $amigo) {
                     if (str_contains(strtolower($amigo['nom']), $valorNombreBusqueda)===true) {
                         $amigosCoincidentes[] = $amigo;
                      }}
-                foreach ($amigosCoincidentes as $amigoCoincidente) {
-                     ?>
-                <div class="col-md-6">
-                  <div class="card row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                      <div class="col p-4 d-flex flex-column position-static">
-                        <h3 class="mb-0"><?=$amigoCoincidente['nom']?></h3>
-                        <h3 class="mb-0"><?=$amigoCoincidente['cognom']?></h3>
+                     if (!empty($amigosCoincidentes)){
+                      foreach ($amigosCoincidentes as $amigoCoincidente) {
+                          ?>
+                      <div class="col-md-6">
+                        <div class="card row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                              <h3 class="mb-0"><?=$amigoCoincidente['nom']?></h3>
+                              <h3 class="mb-0"><?=$amigoCoincidente['cognom']?></h3>
+                            </div>
+                            <div class="col-auto d-none d-lg-block">
+                              <img src="<?=$amigoCoincidente['imatge']?>" alt="imagen" class="bd-placeholder-img" width="300" height="250" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            </div>
+                        </div>
                       </div>
-                      <div class="col-auto d-none d-lg-block">
-                        <img src="<?=$amigoCoincidente['imatge']?>" alt="imagen" class="bd-placeholder-img" width="300" height="250" preserveAspectRatio="xMidYMid slice" focusable="false">
-                      </div>
-                  </div>
-                </div>
-              <?php }
+                    <?php }} 
+                    else {
+                      echo "No hay coincidencias.";
+                    }
               } else if (count($_SESSION['amigos'][0])==0) {
                 echo "No hay amigos creados.";
                 echo "<div class=\"invalid-feedback\">No hay amigos creados.</div>";
